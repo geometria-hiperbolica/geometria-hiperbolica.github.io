9f5f36bb9959d0cb15a5151ad84173dca7b25574
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-05-17 10:26:42 +1000

    new sketch

---
+++ sketches/mobius-transformations/code/math.js
@@ -0,0 +1,249 @@
+/*  mobius-transforms - Exploring Möbius transformations and implementing the book Indra's Pearls
+    Copyright (C) 2022 Tim J. Hutton
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+function p2( x, y ) { return { x:x, y:y, z:0 }; }
+function p3( x, y, z ) { return { x:x, y:y, z:z }; }
+function add( a, b ) { return p3( a.x + b.x, a.y + b.y, a.z + b.z ); }
+function sub( a, b ) { return p3( a.x - b.x, a.y - b.y, a.z - b.z ); }
+function mul( a, f ) { return p3( a.x * f, a.y * f, a.z * f ); }
+function average( a, b ) { return mul( add( a, b ), 0.5 ); }
+function dot( a, b ) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+function len2( a ) { return dot(a,a); }
+function dist2( a, b ) { return len2( sub( a, b ) ); }
+function dist( a, b ) { return Math.sqrt( len2( sub( a, b ) ) ); }
+function mul_complex( a, b ) { return p2( a.x * b.x - a.y * b.y, a.y * b.x + a.x * b.y ); }
+function div_complex( a, b ) { return p2( ( a.x * b.x + a.y * b.y ) / len2( b ), ( a.y * b.x - a.x * b.y ) / len2( b ) ); }
+function negative( a ) { return p2( -a.x, -a.y ); }
+function magnitude( a ) { return Math.sqrt( len2( a ) ); }
+function normalize( a ) { return mul( a, 1 / magnitude( a ) ); }
+function cross( a, b ) { return p3( a.y * b.z - a.z * b.y, a.z*b.x - a.x * b.z, a.x * b.y - a.y * b.x ); }
+function phase( a ) { return Math.atan2( a.y, a.x ); }
+function toPolar( a ) { return [ magnitude( a ), Math.atan2( a.y, a.x ) ]; }
+function fromPolar( mag, phase ) { return p2( mag * Math.cos(phase), mag*Math.sin(phase) ) }
+function sqrt_complex( a ) { return fromPolar( Math.sqrt( magnitude(a) ), phase(a) / 2.0 ); }
+function complex_conjugate( p ) { return p2( p.x, -p.y ); }
+function sphereInversion( p, sphere ) {
+    var d2 = dist2( p, sphere.p );
+    return add( sphere.p, mul( sub( p, sphere.p ), sphere.r * sphere.r / d2 ) );
+}
+function pow_complex( a, p ) {
+    const [ r, theta ] = toPolar( a );
+    return fromPolar( Math.pow( r, p ), theta * p );
+}
+
+function get_mobius_determinant( m ) {
+    return sub( mul_complex( m[0], m[3] ), mul_complex( m[1], m[2] ) );
+}
+
+function mobius_normalize( m ) {
+    // VCA, p.150
+    var sqrt_ad_minus_bc = sqrt_complex( get_mobius_determinant( m ) );
+    for( var i = 0; i < 4; i++ )
+        m[i] = div_complex( m[i], sqrt_ad_minus_bc );
+}
+
+function mobius_make_unitary( m ) {
+    m[2] = mul( complex_conjugate( m[1] ), -1 );
+    m[3] = complex_conjugate( m[0] );
+}
+
+function get_mobius_inverse( m ) {
+    return [m[3], mul(m[1], -1.0), mul(m[2], -1.0), m[0]];
+}
+
+function get_mobius_composed(...args) {
+    return args.reduce((a, b) => {
+        return [add(mul_complex(a[0], b[0]), mul_complex(a[1], b[2])),
+                add(mul_complex(a[0], b[1]), mul_complex(a[1], b[3])),
+                add(mul_complex(a[2], b[0]), mul_complex(a[3], b[2])),
+                add(mul_complex(a[2], b[1]), mul_complex(a[3], b[3]))];
+    });
+}
+
+function mobius_make_nonloxodromic( m ) {
+    m[3].y = -m[0].y;
+}
+
+function mobius_identity( m ) {
+    m[0] = p2( 1, 0 );
+    m[1] = p2( 0, 0 );
+    m[2] = p2( 0, 0 );
+    m[3] = p2( 1, 0 );
+}
+
+function mobius_on_point( m, z ) {
+    if( isNaN(z.x) || isNaN(z.y) ) {
+        // special case for z = inf, return a / c
+        return div_complex( m[0], m[2] );
+    }
+    return div_complex( add( mul_complex( m[0], z ), m[1] ), add( mul_complex( m[2], z ), m[3] ) );
+}
+
+function mobius_on_circle(m, c) {
+    // Apply the mobius transformation to a circle. Indra's Pearls, p. 91
+    var z = sub( c.p, div_complex( p2(c.r * c.r, 0), complex_conjugate( add( div_complex( m[3], m[2] ), c.p) ) ) );
+    //console.log('z:',z);
+    var q = mobius_on_point(m, z);
+    //console.log('q:',q);
+    //console.log('m(p):',mobius_on_point(m, c.p));
+    //console.log('m(p+r):',mobius_on_point(m, add(c.p, p2(c.r, 0.0))));
+    var s = dist(q, mobius_on_point(m, add(c.p, p2(c.r, 0.0))));
+    //console.log('s:',s);
+    return {p:q, r:s};
+}
+
+function get_mobius_trace( m ) {
+    return add( m[0], m[3] );
+}
+
+function get_mobius_fixed_points( T ) {
+    // Indra's Pearls, p. 84, p. 78
+    // returns [ Fix+, Fix- ] (may be the same)
+    var m = [ T[0], T[1], T[2], T[3] ];
+    mobius_normalize( m );
+    var TrT = get_mobius_trace( m );
+    var T2 = get_mobius_composed( m, m );
+    var TrT2 = get_mobius_trace( T2 );
+    var n = mul( add( TrT, sqrt_complex( sub( TrT2, p2( 4.0, 0.0 ) ) ) ), 0.5 );
+    var k = mul_complex( n, n );
+    var st2p4 = sqrt_complex( sub( mul_complex( TrT, TrT ), p2( 4.0, 0.0 ) ) );
+    var z_plus = div_complex( add( sub( m[0], m[3] ), st2p4 ), mul( m[2], 2.0 ) );
+    var z_minus = div_complex( sub( sub( m[0], m[3] ), st2p4 ), mul( m[2], 2.0 ) );
+    if( magnitude( k ) > 1.0 ) { return [ z_plus, z_minus ]; }
+    else { return [ z_minus, z_plus ]; }
+}
+
+function complex_solve_quadratic( a, b, c ) {
+    // return both solutions of ax^2 + bx + c = 0
+    var sqrt_term = sqrt_complex( sub( mul_complex( b, b ), mul( mul_complex( a, c ), 4.0 ) ) );
+    var x1 = div_complex( add( mul( b, -1.0 ), sqrt_term ),  mul( a, 2.0 ) );
+    var x2 = div_complex( sub( mul( b, -1.0 ), sqrt_term ),  mul( a, 2.0 ) );
+    return [ x1, x2 ];
+}
+
+function get_eigenvalues( m ) {
+    return complex_solve_quadratic( p2( 1.0, 0.0 ), mul( add( m[0], m[3] ), -1.0 ), get_mobius_determinant( m ) );
+}
+
+function diagonalize( m ) {
+    const eigenvals = get_eigenvalues( m );
+    const P = [ p2( 1.0, 0.0 ), p2( 1.0, 0.0 ), div_complex( sub( eigenvals[0], m[0] ), m[1] ), div_complex( sub( eigenvals[1], m[0] ), m[1] ) ];
+    const D = [ eigenvals[0], p2( 0.0, 0.0 ), p2( 0.0, 0.0 ), eigenvals[1] ];
+    const Pinv = get_mobius_inverse( P );
+    return [ P, D, Pinv ];
+}
+
+function mobius_power( m, p ) {
+    if( p < 0.0 ) {
+        m = get_mobius_inverse( m );
+        p = Math.abs( p );
+    }
+    //console.log( 'm:', m );
+    const traceM = get_mobius_trace( m );
+    const detM = get_mobius_determinant( m );
+    const trM2_minus_4detM = sub( mul_complex( traceM, traceM ), mul( detM, 4.0 ) );
+    //console.log( 'traceM:', traceM );
+    //console.log( 'detM:', detM );
+    //console.log( 'trM2_minus_4detM:', trM2_minus_4detM );
+    if( magnitude( trM2_minus_4detM ) < 1e-5 ) {
+        // following https://math.berkeley.edu/~ogus/old/Math_54-05/webfoils/jordan.pdf
+        const lambda = mul( get_mobius_trace( m ), 0.5 );
+        //console.log( 'lambda:', lambda );
+        const N = [ sub( m[0], lambda ), m[1], m[2], sub( m[3], lambda ) ]; // N = m - lambda.I
+        //console.log( 'N:', N );
+        var S = [ N[1], p2( 0.0, 0.0 ), N[3], p2( 1.0, 0.0 ) ];
+        if( magnitude( N[1] ) < 1e-5 && magnitude( N[3] ) < 1e-5 ) {
+            //console.log( 'using first column of S' );
+            S = [ N[0], p2( 1.0, 0.0 ), N[2], p2( 0.0, 0.0 ) ];
+        }
+        S[0].x += 1e-6; // fudge factor because some matrices are still problematic
+        //console.log( 'S:', S );
+        //const detS = get_mobius_determinant( S );
+        //console.log( 'detS:', detS );
+        const Sinv = get_mobius_inverse( S );
+        //const Sinv = [ div_complex( Sinv_pre[0], detS ), div_complex( Sinv_pre[1], detS ), div_complex( Sinv_pre[2], detS ), div_complex( Sinv_pre[3], detS ) ];
+        //const L = [ lambda, p2( 1.0, 0.0 ), p2( 0.0, 0.0 ), lambda ];
+        //const SLSinv = get_mobius_composed( S, L, Sinv );
+        const Lp = [ pow_complex( lambda, p ), mul( lambda, p ), p2( 0.0, 0.0 ), pow_complex( lambda, p ) ];
+        return get_mobius_composed( S, Lp, Sinv );
+    }
+    const [ P, D, Pinv ] = diagonalize( m );
+    const D2 = [ pow_complex( D[0], p ), p2( 0.0, 0.0 ), p2( 0.0, 0.0 ), pow_complex( D[3], p ) ];
+    return get_mobius_composed( P, D2, Pinv );
+}
+
+function complex_apply_quadratic( x, a, b, c ) {
+    return add( add( mul_complex( a, mul_complex( x, x ) ), mul_complex( b, x ) ), c );
+}
+
+function pair_circles(a, b) {
+    // return the Mobius transformation that pairs these two circles. Indra's Pearls, p.90
+    var P = a.p;
+    var r = a.r;
+    var Q = b.p;
+    var s = b.r;
+    // DEBUG:
+    //return [ p2(1.0, 0.0), negative(P), p2(0.0, 0.0), p2(1.0, 0.0) ]; // z - P
+    //return [ p2(0.0, 0.0), r*s, p2(1.0, 0.0), negative(P) ]; // rs / (z - P)
+    return [ Q, sub( p2(r * s, 0), mul_complex(Q, P) ), p2(1.0, 0.0), negative( P ) ]; // rs / (z - P) + Q
+}
+
+function pair_circles2(a, b, u, v) {
+    // return the Mobius transformation that pairs these two circles. Indra's Pearls, p.90
+    var P = a.p;
+    var r = a.r;
+    var Q = b.p;
+    var s = b.r;
+    return [ add( mul_complex(p2(s, 0.0), complex_conjugate( v )), mul_complex(Q, u)),
+             sub( sub( add( mul(complex_conjugate(u), s*r), mul_complex(Q, mul(v, r)) ), mul( mul_complex(complex_conjugate(v), P), s ) ), mul_complex(mul_complex(Q,u), P) ),
+             u,
+             sub( mul_complex( p2(r, 0.0), v ), mul_complex(u, P) ) ];
+}
+
+function pointInRect( p, rect ) {
+    return p.x > rect.x && p.x < ( rect.x + rect.width ) &&
+           p.y > rect.y && p.y < ( rect.y + rect.height );
+}
+
+function distanceOfPointFromLine( end1, end2, p ) {
+    return Math.abs( (end2.y-end1.y)*p.x - (end2.x-end1.x)*p.y + end2.x*end1.y - end2.y*end1.x ) / dist( end1, end2 );
+}
+
+function roundTowardsZero( x ) {
+    return ( x > 0 ) ? Math.floor(x) : Math.ceil(x);
+}
+
+function find_line_segments_intersection( a, b, c, d ) {
+    var dx1 = b.x - a.x;
+    var dy1 = b.y - a.y;
+    var dx2 = d.x - c.x;
+    var dy2 = d.y - c.y;
+    var dx3 = a.x - c.x;
+    var dy3 = a.y - c.y;
+    var d = dx1 * dy2 - dx2 * dy1;
+    if(d !== 0) {
+        var s = dx1 * dy3 - dx3 * dy1;
+        if((s <= 0 && d < 0 && s >= d) || (s >= 0 && d > 0 && s <= d)) {
+            var t = dx2 * dy3 - dx3 * dy2;
+            if((t <= 0 && d < 0 && t > d) || (t >= 0 && d > 0 && t < d)) {
+                t = t / d;
+                return [true, p2( a.x + t * dx1, a.y + t * dy1 ) ];
+            }
+        }
+    }
+    return [false, undefined];
+}
---
+++ sketches/mobius-transformations/code/recipes.js
@@ -0,0 +1,247 @@
+/*  mobius-transforms - Exploring Möbius transformations and implementing the book Indra's Pearls
+    Copyright (C) 2022 Tim J. Hutton
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+function get_recipes()
+{
+    // start_letters: default is [0, 3, 2, 1] ie. aBAb
+
+    const prec = 6;
+
+    var fuchsian = { id: 'fuchsian', label: "Fuchsian", control_points: [], pt_labels: [], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, page 163
+            var transforms = [];
+            var root2 = Math.sqrt( 2.0 );
+            transforms[0] = [ p2(root2, 0.0), p2(0.0, 1.0), p2(0.0, -1.0), p2(root2, 0.0) ];
+            transforms[1] = [ p2(root2, 0.0), p2(1.0, 0.0), p2(1.0, 0.0),  p2(root2, 0.0) ];
+            var description = "Fuchsian recipe from Indra's Pearls, p. 163.";
+            return [transforms, description];
+        }
+    };
+    var theta_schottky = { id: 'thetaSchottky', label: "\u03B8-Schottky",
+        control_points: [ p2(0.6, 0.5) ], pt_labels: [ '\u03B8' ], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, page 118
+            var theta = Math.atan2( control_points[0].y, control_points[0].x );
+            var sin_theta = Math.sin( theta );
+            var cos_theta = Math.cos( theta );
+            var transforms = [];
+            transforms[0] = [ p2( 1.0 / sin_theta, 0.0 ), p2( 0.0, cos_theta / sin_theta), p2( 0.0, -cos_theta / sin_theta ), p2( 1.0 / sin_theta, 0.0 ) ];
+            transforms[1] = [ p2( 1.0 / sin_theta, 0.0 ), p2( cos_theta / sin_theta, 0.0 ), p2( cos_theta / sin_theta, 0.0 ), p2( 1.0 / sin_theta, 0.0 ) ];
+            var description = `\u03B8-Schottky recipe from Indra's Pearls with \u03B8 = ${theta.toFixed(prec)}`;
+            return [transforms, description];
+        }
+    };
+
+    var kissing_schottky = { id: 'kissingSchottky', label: "kissing Schottky",
+        control_points: [ p2(0.91, 1.08) ], pt_labels: [ 'yv' ], num_solutions: 2,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 170
+            var y = control_points[0].x;
+            var v = control_points[0].y;
+            var x = Math.sqrt( 1.0 + y * y );
+            var u = Math.sqrt( 1.0 + v * v );
+            var s = 2.0 / ( y * v );
+            var t = Math.max( 0.0, s * s - 4.0 ); // (not sure how to handle complex k)
+            var k = ( s + [1.0, -1.0][which_solution] * Math.sqrt( t ) ) / 2.0;
+            var transforms = [];
+            transforms[0] = [ p2(x, 0.0), p2(y, 0.0), p2(y, 0.0), p2(x, 0.0) ];
+            transforms[1] = [ p2(u, 0.0), p2(0.0, k * v), p2(0.0, -v / k), p2(u, 0.0) ];
+            var description = `Kissing Schottky recipe from Indra's Pearls, p. 170. With x = ${x.toFixed(prec)}, y = ${y.toFixed(prec)}, u = ${u.toFixed(prec)}, v = ${v.toFixed(prec)}, k = ${k.toFixed(prec)}`;
+            return [transforms, description];
+        }
+    };
+
+    var gasket = { id: 'gasket', label: "gasket", control_points: [], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            var transforms = [];
+            transforms[0] = [ p2( 1.0, 0.0 ), p2( 0.0, 0.0), p2( 0.0, -2.0 ), p2( 1.0, 0.0 ) ];
+            transforms[1] = [ p2( 1.0, -1.0 ), p2( 1.0, 0.0 ), p2( 1.0, 0.0 ), p2( 1.0, 1.0 ) ];
+            var description = "Gasket recipe from Indra's Pearls, p. 201";
+            return [transforms, description];
+        },
+        start_letters: [0],
+        duplicate: 'rot180'
+    };
+
+    var modular = { id: 'modular', label: "modular group", control_points: [], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            var transforms = [];
+            transforms[0] = [ p2( 1.0, 0.0 ), p2( 0.0, 0.0), p2( -2.0, 0.0 ), p2( 1.0, 0.0 ) ];
+            transforms[1] = [ p2( 1.0, 0.0 ), p2( 2.0, 0.0 ), p2( 0.0, 0.0 ), p2( 1.0, 0.0 ) ];
+            var description = "The modular group recipe from Indra's Pearls, p. 214";
+            return [transforms, description];
+        }
+    };
+
+    var maskit = { id: 'maskit', label: "Maskit recipe",
+        control_points: [ p2(0, 2.0) ], pt_labels: [ 'mu' ], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 259
+            var mu = control_points[0];
+            var transforms = [];
+            transforms[0] = [ mu, p2(1.0, 0.0), p2(1.0, 0.0), p2(0.0, 0.0) ];              // a: z -> mu + 1/z = (mu*z+1)/z
+            transforms[1] = [ p2(1.0, 0.0), p2(2.0, 0.0), p2(0.0, 0.0),  p2(1.0, 0.0) ];   // b: z -> z + 2
+            var description = "Maskit recipe from Indra's Pearls, p. 259 with \u03BC = " + format_complex(mu);
+            return [transforms, description];
+        },
+        start_letters: [0],
+        duplicate: 'maskit'
+    };
+
+    var maskit2 = { id: 'maskit2', label: "Maskit recipe 2",
+        control_points: [ p2(0, 2.0) ], pt_labels: [ 'mu' ], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 259, plus Jos Leys' b-variation (http://www.josleys.com/articles/Kleinian%20escape-time_3.pdf)
+            var mu = control_points[0];
+            var k = p2(2.0 * Math.cos(Math.PI / 5.0), 0.0);
+            var transforms = [];
+            transforms[0] = [ mu, p2(1.0, 0.0), p2(1.0, 0.0), p2(0.0, 0.0) ];      // a: z -> mu + 1/z = (mu*z+1)/z
+            transforms[1] = [ p2(1.0, 0.0), k, p2(0.0, 0.0),  p2(1.0, 0.0) ];      // b: z -> z + k
+            var description = "Maskit recipe from Indra's Pearls, p. 259 with \u03BC = " + format_complex(mu) + ", plus <a href=\"http://www.josleys.com/articles/Kleinian%20escape-time_3.pdf\">Jos Leys' b-variation</a> with k = 2cos(pi/5)";
+            return [transforms, description];
+        }
+    };
+
+    var maskit3 = { id: 'maskit3', label: "Maskit recipe 3",
+        control_points: [ p2(0, 2.0) ], pt_labels: [ 'mu' ], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 259, plus Jos Leys' b-variation (http://www.josleys.com/articles/Kleinian%20escape-time_3.pdf)
+            var mu = control_points[0];
+            var k = p2(2.0 * Math.cos(Math.PI / 4.0), 0.0);
+            var transforms = [];
+            transforms[0] = [ mu, p2(1.0, 0.0), p2(1.0, 0.0), p2(0.0, 0.0) ];      // a: z -> mu + 1/z = (mu*z+1)/z
+            transforms[1] = [ p2(1.0, 0.0), k, p2(0.0, 0.0),  p2(1.0, 0.0) ];      // b: z -> z + k
+            var description = "Maskit recipe from Indra's Pearls, p. 259 with \u03BC = " + format_complex(mu) + ", plus <a href=\"http://www.josleys.com/articles/Kleinian%20escape-time_3.pdf\">Jos Leys' b-variation</a> with k = 2cos(pi/4)";
+            return [transforms, description];
+        }
+    };
+
+    var grandma = { id: 'grandma',
+        label: "Grandma's recipe",
+         num_solutions: 2,
+        control_points: [ p2(2.4, 0.0), p2(2.37, 0.34) ],
+        //control_points: [ p2(1.87, 0.1), p2(1.87, -0.1) ], // for debugging the necks
+        //control_points: [ p2(2.0, -1.0), p2(3.0, 0.0) ], // Fig. 8.16, p. 247, needing aaB
+        //control_points: [ p2(1.958591030,-0.011278560), p2(2.0, 0.0) ], //Fig. 9.1, p. 269
+        //control_points: [ p2(1.64213876,-0.76658841), p2(2.0, 0.0) ], //Fig. 9.3, p. 272
+        //control_points: [ p2(0.136998688,1.80785524), p2(2.0, 0.0) ], //Fig. 9.16, p. 295 ?
+        //control_points: [ p2(1.5306639,-0.8501047), p2(1.5306639,0.8501047) ], //Fig. 9.19, p. 298
+        pt_labels: [ 'ta', 'tb' ],
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 227
+            var t_a = control_points[0];
+            var t_b = control_points[1];
+            // solve x^2 - t_a * t_b * x + t_a^2 + t_b^2 = 0 for x
+            var solutions = complex_solve_quadratic( p2( 1.0, 0.0 ), mul( mul_complex( t_a, t_b ), -1.0 ), add( mul_complex( t_a, t_a ), mul_complex( t_b, t_b ) ) );
+            var t_ab = solutions[which_solution]; // pick one
+            var z0 = div_complex( mul_complex( sub( t_ab, p2( 2.0, 0.0 ) ), t_b ),
+                                  add( sub( mul_complex( t_b, t_ab ), mul( t_a, 2.0 ) ), mul_complex( p2( 0.0, 2.0 ), t_ab ) ) );
+            var transforms = [];
+            transforms[0] = [ mul( t_a, 0.5 ),
+                              div_complex( add( sub( mul_complex( t_a, t_ab ), mul( t_b, 2.0 ) ), p2( 0.0, 4.0 ) ), mul_complex( add( mul( t_ab, 2.0 ), p2( 4.0, 0.0 ) ), z0 ) ),
+                              div_complex( mul_complex( sub( sub( mul_complex( t_a, t_ab ), mul( t_b, 2.0 ) ), p2( 0.0, 4.0 ) ), z0 ), sub( mul( t_ab, 2.0 ), p2( 4.0, 0.0 ) ) ),
+                              mul( t_a, 0.5 ) ];
+            transforms[1] = [ mul( sub( t_b, p2( 0.0, 2.0 ) ), 0.5 ), mul( t_b, 0.5 ), mul( t_b, 0.5 ), mul( add( t_b, p2( 0.0, 2.0 ) ), 0.5 ) ];
+            var description = "Grandma's recipe from Indra's Pearls, p. 227. With t<sub>a</sub> = " + format_complex(t_a) + ", t<sub>b</sub> = " + format_complex(t_b);
+            return [transforms, description];
+        },
+        start_letters: [0],
+        duplicate: 'rot180'
+    };
+
+    var riley = { id: 'riley', label: "Riley's recipe",
+        control_points: [ p2(0.1, 0.93) ], pt_labels: [ 'c' ], num_solutions: 1,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 258
+            var c = control_points[0];
+            var transforms = [];
+            transforms[0] = [ p2( 1.0, 0.0 ), p2( 0.0, 0.0 ), c,              p2( 1.0, 0.0 ) ];
+            transforms[1] = [ p2( 1.0, 0.0 ), p2( 2.0, 0.0 ), p2( 0.0, 0.0 ), p2( 1.0, 0.0 ) ];
+            var description = "Riley's recipe from Indra's Pearls, p. 258. With c = " + format_complex(c);
+            return [transforms, description];
+        },
+        start_letters: [0],
+        duplicate: 'rot180_and_rep_x_2'
+    };
+
+    var jorgensen = { id: 'jorgensen', label: "Jørgensen's recipe",
+        control_points: [ p2(1.87, 0.1), p2(1.87, -0.1) ], pt_labels: [ 'ta', 'tb' ], num_solutions: 2,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 256
+            var t_a = control_points[0];
+            var t_b = control_points[1];
+            // solve x^2 - t_a * t_b * x + t_a^2 + t_b^2 = 0 for x
+            var solutions = complex_solve_quadratic( p2( 1.0, 0.0 ), mul( mul_complex( t_a, t_b ), -1.0 ), add( mul_complex( t_a, t_a ), mul_complex( t_b, t_b ) ) );
+            var t_ab = solutions[which_solution]; // pick one
+            var transforms = [];
+            transforms[0] = [ sub( t_a, div_complex( t_b, t_ab ) ), div_complex( t_a, mul_complex( t_ab, t_ab ) ), t_a, div_complex( t_b, t_ab ) ];
+            transforms[1] = [ sub( t_b, div_complex( t_a, t_ab ) ), mul( div_complex( t_b, mul_complex( t_ab, t_ab ) ), -1.0 ), mul( t_b, -1.0 ), div_complex( t_a, t_ab ) ];
+            var description = "Jørgensen's recipe from Indra's Pearls, p. 256. With t<sub>a</sub> = " + format_complex(t_a) + ", t<sub>b</sub> = " + format_complex(t_b);
+            return [transforms, description];
+        }
+        // TODO: find a stretch of letters that can be duplicated
+    };
+
+    var special = { id: 'special',
+        label: "Grandma's four-alarm special",
+        //control_points: [ p2(1.87, -0.08), p2(1.87, 0.1), p2(-1.87, 0.05) ],
+        control_points: [ p2(1.87, 0.08), p2(1.87, -0.1), p2(1.79, -1.948) ], // discovered
+        //control_points: [ p2(1.863, -0.072), p2(1.900, 0.1), p2(1.79, 1.948) ], // discovered
+        //control_points: [ p2(1.924781, -0.047529), p2(2.0, 0.0), p2(0.0, 0.0) ], // Fig. 11.1, p. 354 ?
+        pt_labels: [ 'ta', 'tb', 'tab' ],
+        num_solutions: 2,
+        make_generators: (which_solution, control_points) => {
+            // Indra's Pearls, p. 260
+            // not sure this is working correctly. will need to explore when have better rendering.
+            var t_a = control_points[0];
+            var t_b = control_points[1];
+            var t_ab = control_points[2];
+            var t_C = sub( sub( add( add( mul_complex( t_a, t_a ), mul_complex( t_b, t_b ) ), mul_complex( t_ab, t_ab ) ),
+                mul_complex( mul_complex( t_a, t_b ), t_ab ) ), p2(2.0, 0.0) );
+            var Q = mul( sqrt_complex( sub( p2( 2.0, 0.0 ), t_C ) ), which_solution==0?1.0:-1.0 );
+            var sign = 1.0;
+            var sqrt_tc_plus_2 = sqrt_complex( add( t_C, p2( 2.0, 0.0 ) ) );
+            if( magnitude( add( t_C, mul_complex( mul_complex( p2( 0.0, 1.0 ), Q ), sqrt_tc_plus_2 ) ) ) < 2.0 ) {
+                sign = -1.0;
+            }
+            var R = mul( sqrt_tc_plus_2, sign );
+            var z0 = div_complex( mul_complex( sub( t_ab, p2( 2.0, 0.0 ) ), add( t_b, R ) ), add( sub( mul_complex( t_b, t_ab ), mul( t_a, 2.0 ) ),
+                mul_complex( mul_complex( p2( 0.0, 1.0 ), Q ), t_ab ) ) );
+            var transforms = [];
+            transforms[0] = [
+                mul( t_a, 0.5 ),
+                div_complex( add( sub( mul_complex( t_a, t_ab ), mul( t_b, 2.0 ) ), mul_complex( p2( 0.0, 2.0 ), Q ) ), mul_complex( add( mul( t_ab, 2.0 ), p2( 4.0, 0.0 ) ), z0 ) ),
+                div_complex( mul_complex( sub( sub( mul_complex( t_a, t_ab ), mul( t_b, 2.0 ) ), mul_complex( p2( 0.0, 2.0 ), Q ) ), z0 ), sub( mul( t_ab, 2.0 ), p2( 4.0, 0.0 ) ) ),
+                mul( t_a, 0.5 ) ];
+            transforms[1] = [
+                mul( sub( t_b, mul_complex( p2( 0.0, 1.0 ), Q ) ), 0.5 ),
+                div_complex( sub( sub( mul_complex( t_b, t_ab ), mul( t_a, 2.0 ) ), mul_complex( mul_complex( p2( 0.0, 1.0 ), Q ), t_ab ) ),
+                    mul_complex( add( mul( t_ab, 2.0 ), p2( 4.0, 0.0 ) ), z0 ) ),
+                div_complex( mul_complex( add( sub( mul_complex( t_b, t_ab ), mul( t_a, 2.0 ) ), mul_complex( mul_complex( p2( 0.0, 1.0 ), Q ), t_ab ) ), z0 ),
+                    sub( mul( t_ab, 2.0 ), p2( 4.0, 0.0 ) ) ),
+                mul( add( t_b, mul_complex( p2( 0.0, 1.0 ), Q ) ), 0.5 ) ];
+            var description = "Grandma's four alarm special from Indra's Pearls, p. 260. With t<sub>a</sub> = " + format_complex(t_a) + ", t<sub>b</sub> = " + format_complex(t_b)
+                + ", t<sub>ab</sub> = " + format_complex(t_ab);
+            return [transforms, description];
+        },
+        start_letters: [0],
+        duplicate: 'rot180'
+    };
+
+    return [ fuchsian, theta_schottky, kissing_schottky, gasket, modular, maskit, maskit2, maskit3, grandma, riley, jorgensen, special ];
+}
